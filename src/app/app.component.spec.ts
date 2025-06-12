2aaec3c14a4184db1ea929027fd660ff1d836cb7
Parthavi <parthavi.patel@cmarix.com>
2025-06-12 13:07:16 +0530

    Changed in login html file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,7 +13,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruth();
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'TaskManagementSystem'`, () => {
