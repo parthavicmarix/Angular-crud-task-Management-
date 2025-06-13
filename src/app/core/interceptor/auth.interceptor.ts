134c0bebcb435c8841d17713dc67cdc644e2581c
Parthavi <parthavi.patel@cmarix.com>
2025-06-13 13:09:20 +0530

    1.4

--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -12,7 +12,6 @@ import { environment } from 'src/environments/environment';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() { }
 
   intercept(httpRequest: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let request = httpRequest;
