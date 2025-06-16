6d03daedc34854ecd594843c4b7f27f6a5abccd2
Parthavi <parthavi.patel@cmarix.com>
2025-06-16 11:48:40 +0530

    005

--- src/app/shared/components/common-snackbar/common-snackbar.component.ts
+++ src/app/shared/components/common-snackbar/common-snackbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
-import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-common-snackbar',
